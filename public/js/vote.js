1348ba60e92051c81619d45b4338e5f2e88bf9f3
zhren-byte <chavez.thomas970@gmail.com>
2023-12-25 23:33:20 -0300

    Female and Male

---
+++ public/js/vote.js
@@ -0,0 +1,53 @@
+document.addEventListener("keydown", (e) => {
+  e.preventDefault();
+  if (e.key == "ArrowLeft") return voteImg($(".images")[0]);
+  if (e.key == "ArrowRight") return voteImg($(".images")[1]);
+});
+$("input").keypress(function () {
+  $("span").text((i += 1));
+});
+function voteImg(e) {
+  const winner = $(e).attr("alt");
+  const loser = $(".images").not(e).attr("alt");
+  const winnerWins = localStorage.getItem(`${winner}_wins`) || 0;
+  localStorage.setItem(`${winner}_wins`, parseInt(winnerWins) + 1);
+
+  if (parseInt(winnerWins) + 1 >= 5) return updateImages(winner);
+  $.ajax({
+    type: "POST",
+    url: "/vote/male",
+    data: {
+      winner: winner,
+      loser: loser,
+    },
+    success: function (res) {
+      $(".images").not(
+        e
+      )[0].outerHTML = `<img onclick="voteImg(this)" class="images" width="500" height="500" src="${
+        "https://this-person-does-not-exist.com/" + res.url
+      }" alt="${res.name}" />`;
+    },
+  });
+}
+async function updateImages(winner) {
+  // Realizar una solicitud al servidor para obtener nuevas imágenes
+  try {
+    const response = await fetch("/get-new-images"); // Asegúrate de tener una ruta que devuelva nuevas imágenes en el servidor
+    const newImages = await response.json();
+
+    $(
+      ".images"
+    )[0].outerHTML = `<img onclick="voteImg(this)" class="images" width="500" height="500" src="${
+      "https://this-person-does-not-exist.com/" + newImages[0].url
+    }" alt="${newImages[0].url}" />`;
+    $(
+      ".images"
+    )[1].outerHTML = `<img onclick="voteImg(this)" class="images" width="500" height="500" src="${
+      "https://this-person-does-not-exist.com/" + newImages[1].url
+    }" alt="${newImages[1].url}" />`;
+
+    localStorage.setItem(`${winner}_wins`, 0);
+  } catch (error) {
+    console.error("Error al obtener nuevas imágenes:", error);
+  }
+}
--- src/index.js
+++ src/index.js
@@ -17,26 +17,33 @@ mongoose.connection.on('disconnected', () => {
   console.warn("Mongoose connection lost");
 });
 
-const imageSchema = new mongoose.Schema({
+const femaleSchema = new mongoose.Schema({
+  url: String,
+  name: String,
+  rating: Number,
+});
+const maleSchema = new mongoose.Schema({
   url: String,
   name: String,
   rating: Number,
 });
 
-const Image = mongoose.model('Image', imageSchema); 
+const Female = mongoose.model('Female', femaleSchema); 
+const Male = mongoose.model('Male', maleSchema); 
 const K = 32;
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/styles", express.static('public/styles'));
+app.use("/js", express.static('public/js'));
 app.set('view engine', 'hbs');
 app.set("views", __dirname + "/views");
 
 app.get("/", async (req, res) => {
   try {
-    const randomImage = await Image.aggregate([{ $sample: { size: 2 } }]);
+    const randomImage = await Male.aggregate([{ $sample: { size: 2 } }]);
     res.render('index', {
-      titulo: 'Faces Mash',
+      titulo: 'FakeMash',
       girlImg1: `https://this-person-does-not-exist.com/${randomImage[0].url}`,
       girl1: randomImage[0].name,
       girlImg2: `https://this-person-does-not-exist.com/${randomImage[1].url}`,
@@ -49,13 +56,50 @@ app.get("/", async (req, res) => {
 });
 
 // Manejar la elección del usuario
-app.post("/vote", async (req, res) => {
+app.get("/get-new-images", async (req, res) => {
+  try {
+    const randomImage = await Female.aggregate([{ $sample: { size: 2 } }]);
+    res.send([randomImage[0], randomImage[1]])
+  } catch (error) {
+    console.error('Error al procesar el voto:', error);
+    res.status(500).send('Error interno del servidor');
+  }
+});
+// Manejar la elección del usuario
+app.post("/vote/female", async (req, res) => {
   try {
     const { winner, loser } = req.body;
 
     // Recupera las imágenes desde la base de datos
-    const imageWinner = await Image.findOne({ name: winner });
-    const imageLoser = await Image.findOne({ name: loser });
+    const imageWinner = await Female.findOne({ name: winner });
+    const imageLoser = await Female.findOne({ name: loser });
+
+    // Actualiza las calificaciones basadas en el resultado del voto
+    if (imageWinner && imageLoser) {
+      // Calcula las expectativas (Ea y Eb) utilizando la fórmula de Elo rating
+      const Ea = 1 / (1 + 10 ** ((imageLoser.rating - imageWinner.rating) / 400));
+      const Eb = 1 / (1 + 10 ** ((imageWinner.rating - imageLoser.rating) / 400));
+      imageWinner.rating += K * (1 - Ea);
+      imageLoser.rating += K * (0 - Eb);
+      // Guarda las nuevas calificaciones en la base de datos
+      await imageWinner.save();
+      await imageLoser.save();
+    }
+    // Devuelve un documento de la base de datos
+    const randomImage = await Female.aggregate([{ $sample: { size: 1 } }]);
+    res.send(randomImage[0])
+  } catch (error) {
+    console.error('Error al procesar el voto:', error);
+    res.status(500).send('Error interno del servidor');
+  }
+});
+app.post("/vote/male", async (req, res) => {
+  try {
+    const { winner, loser } = req.body;
+
+    // Recupera las imágenes desde la base de datos
+    const imageWinner = await Male.findOne({ name: winner });
+    const imageLoser = await Male.findOne({ name: loser });
 
     // Actualiza las calificaciones basadas en el resultado del voto
     if (imageWinner != undefined && imageLoser != undefined) {
@@ -69,7 +113,7 @@ app.post("/vote", async (req, res) => {
       await imageLoser.save();
     }
     // Devuelve un documento de la base de datos
-    const randomImage = await Image.aggregate([{ $sample: { size: 1 } }]);
+    const randomImage = await Male.aggregate([{ $sample: { size: 1 } }]);
     res.send(randomImage[0])
   } catch (error) {
     console.error('Error al procesar el voto:', error);
@@ -77,36 +121,66 @@ app.post("/vote", async (req, res) => {
   }
 });
 
-app.get("/generate-images/:count", async (req, res) => {
+app.get("/generate-images/female/:count", async (req, res) => {
   try {
     const count = parseInt(req.params.count);
     for (let i = 0; i < count; i++) {
       const response = await axios.get('https://this-person-does-not-exist.com/new?gender=female&age=19-25&etnic=white');
-      const image = await Image.findOneAndUpdate(
+      const female = await Female.findOneAndUpdate(
         { url: response.data.src },
-        { $setOnInsert: { url: response.data.src, name: faker.person.firstName('female'), rating: 1000 } },
+        { $setOnInsert: { url: response.data.src, name: faker.person.fullName({ sex: 'female' }), rating: 1000 } },
         { upsert: true, new: true }
       );
       // Enviar actualización periódica al cliente
-      res.write(`Generando imagen > ${image.name}: ${image} ${i + 1} / ${count}\n`);
+      // res.write(`Generando imagen > ${image.name}: ${image} ${i + 1} / ${count}\n`);
     }
-    res.end()
+    res.send("Generacion Finalizada")
   } catch (error) {
     console.error('Error al crear las imágenes:', error);
-    res.status(500).send('Error interno del servidor');
+  }
+});
+app.get("/generate-images/male/:count", async (req, res) => {
+  try {
+    const count = parseInt(req.params.count);
+    for (let i = 0; i < count; i++) {
+      const response = await axios.get('https://this-person-does-not-exist.com/new?gender=male&age=19-25&etnic=white');
+      const male = await Male.findOneAndUpdate(
+        { url: response.data.src },
+        { $setOnInsert: { url: response.data.src, name: faker.person.fullName({ sex: 'male' }), rating: 1000 } },
+        { upsert: true, new: true }
+      );
+    }
+    res.send("Generacion Finalizada")
+  } catch (error) {
+    console.error('Error al crear las imágenes:', error);
   }
 });
 
-app.get("/ranking", async (req, res) => {
+app.get("/ranking/female", async (req, res) => {
   try {
     // Obtener las 10 imágenes con el Elo más alto
-    const topImages = await Image.find().sort({ rating: -1 }).limit(10);
-    const cleanedTopImages = topImages.map(image => ({
-      ...image.toObject(),
-      name: image.name.replace(/^avatar-gen/, '').replace(/\.jpg$/, '')
-    }));
+    const topImages = await Female.find().sort({ rating: -1 }).limit(10);
+    // const cleanedTopImages = topImages.map(image => ({
+    //   ...image.toObject(),
+    //   name: image.name.replace(/^avatar-gen/, '').replace(/\.jpg$/, '')
+    // }));
     // Renderizar la plantilla de ranking con la lista de imágenes
-    res.render('ranking', { titulo: 'Top 10 Elo Ranking', ranking: cleanedTopImages });
+    res.render('ranking', { titulo: 'Top 10 Elo Ranking Female', ranking: topImages });
+  } catch (error) {
+    console.error('Error al obtener el ranking:', error);
+    res.status(500).send('Error interno del servidor');
+  }
+});
+app.get("/ranking/male", async (req, res) => {
+  try {
+    // Obtener las 10 imágenes con el Elo más alto
+    const topImages = await Male.find().sort({ rating: -1 }).limit(10);
+    // const cleanedTopImages = topImages.map(image => ({
+    //   ...image.toObject(),
+    //   name: image.name.replace(/^avatar-gen/, '').replace(/\.jpg$/, '')
+    // }));
+    // Renderizar la plantilla de ranking con la lista de imágenes
+    res.render('ranking', { titulo: 'Top 10 Elo Ranking Male', ranking: topImages });
   } catch (error) {
     console.error('Error al obtener el ranking:', error);
     res.status(500).send('Error interno del servidor');
